13bbfd8acdbd3fc1b4c8f0a3718b5a8c47cd14e3
pivs96 <jpalaznik@gmail.com>
2016-05-03 17:30:45 +0300

    Make md5 from apache commons

--- src/main/java/by/palaznik/codecomplete/controller/FileServlet.java
+++ src/main/java/by/palaznik/codecomplete/controller/FileServlet.java
@@ -9,11 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import java.util.Base64;
 
 @WebServlet("/file")
 public class FileServlet extends HttpServlet {
@@ -23,42 +18,17 @@ public class FileServlet extends HttpServlet {
         int num = Integer.valueOf(request.getParameter("num"));
         String hash = request.getParameter("checksum");
         String dataBase64 = request.getParameter("data");
-        System.out.println(num);
+        System.out.print(num + ": " + hash + ", ");
 
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
         PrintWriter out = response.getWriter();
 
-        String data = new String(Base64.getDecoder().decode(dataBase64), StandardCharsets.UTF_8);
-        if (checkHash(data, hash)) {
+        if (FileService.checkHash(dataBase64, hash)) {
             out.print("OK");
         } else {
             out.print("REPEAT");
         }
         out.flush();
-    }
-
-    private boolean checkHash(String data, String hash) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(data.getBytes()); // Change this to "UTF-16" if needed
-            byte[] digest = md.digest();
-
-            StringBuffer hashString = new StringBuffer();
-            for (byte number : digest) {
-                hashString.append(Integer.toString((number & 0xff) + 0x100, 16).substring(1));
-            }
-
-            System.out.println("Data hash: " + hashString);
-            System.out.println("Real hash: " + hash);
-
-            if (hash.equals(hashString.toString())) {
-                return true;
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        return false;
     }
 }
--- src/main/java/by/palaznik/codecomplete/service/FileService.java
+++ src/main/java/by/palaznik/codecomplete/service/FileService.java
@@ -1,33 +1,16 @@
 package by.palaznik.codecomplete.service;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class FileService {
 
-//    public boolean checkHash(String data, String hash) {
-//        try {
-//            MessageDigest md = MessageDigest.getInstance("MD5");
-//            md.update(data.getBytes()); // Change this to "UTF-16" if needed
-//            byte[] digest = md.digest();
-//            byte[] hashBytes = hash.getBytes();
-//            for (byte num : digest) {
-//                System.out.print(num);
-//            }
-//            System.out.println();
-//            for (byte num : hashBytes) {
-//                System.out.print(num);
-//            }
-//            System.out.println(digest);
-//            System.out.println(hashBytes);
-//            if (Arrays.equals(digest, hashBytes)) {
-//                return true;
-//            }
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return false;
-//    }
+    public static boolean checkHash(String dataBase64, String hash) {
+        String data = new String(Base64.getDecoder().decode(dataBase64), StandardCharsets.UTF_8);
+        System.out.println(data);
+        String dataHash = DigestUtils.md5Hex(data);
+        return hash.equals(dataHash);
+    }
 }
